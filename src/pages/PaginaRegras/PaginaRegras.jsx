9666567196f7812290ae596569092c7693be6b8d
schddan <danielschmitzsc@gmail.com>
2024-07-18 12:54:24 -0300

    Alterações na página de regras, com adição de hover nas cartas e alinhamento do texto

--- src/pages/PaginaRegras/PaginaRegras.jsx
+++ src/pages/PaginaRegras/PaginaRegras.jsx
@@ -6,18 +6,27 @@ export default function PaginaRegras() {
     return (
         <div>
             <NavBar />
-            <div className="cartasConjunto">
-                <CartaInfo img={"src/assets/mais4.png"} nome={"Comprar Quatro Cartas"} texto={"Faz comprar 4"} />
-                <CartaInfo img={"src/assets/mudarCor.png"} nome={"Curinga"} texto={"Faz comprar 4"} />
-                <CartaInfo img={"src/assets/vermelha0.png"} nome={"Cartas de cor numeradas"} texto={"Faz comprar 4"} />
-                <CartaInfo img={"src/assets/azulMais2.png"} nome={"Comprar Duas Cartas"} texto={"Faz comprar 4"} />
-                <CartaInfo img={"src/assets/verdeReverter.png"} nome={"Inverter"} texto={"Faz comprar 4"} />
-                <CartaInfo img={"src/assets/amareloBloquear.png"} nome={"Bloquear"} texto={"Faz comprar 4"} />
+            <div className="regrasContent">
+                <div className="penalidades">
+                    <h2>Penalidades</h2>
+                    <p><strong>1.</strong> Caso o jogador fique com uma carta na mão e não grite UNO, ele deve comprar 2 cartas. Entretanto, é necessário que alguém perceba e acuse antes do próximo jogador começar a jogada</p>
+                    <p><strong>2.</strong> Caso o jogador jogue uma carta errada no monte e alguém perceba, ele deve retirar a carta e comprar duas do monte</p>
+                    <p><strong>3.</strong> Jogadores que derem palpites devem comprar duas cartas do monte</p>
+                </div>
+
+                <div className="parteDasCartas">
+                    <h2>Cartas</h2>
+                    <div className="cartasConjunto">
+                        <CartaInfo img={"src/assets/mais4.png"} nome={"Comprar Quatro Cartas"} texto={"Permite que o jogador escolha a cor com que o jogo deve seguir, além de obrigar o jogador seguinte a comprar quatro cartas e perder a vez. Esta carta não pode ser a carta inicial do jogo, portanto, se for retirada do monte para começar, deve ser devolvida"} />
+                        <CartaInfo img={"src/assets/verdeReverter.png"} nome={"Inverter"} texto={"Como o próprio nome sugere, esta carta inverte a ordem do jogo. Se esta indo no sentido horário, passará a ser no sentido anti-horário e vice-versa. Também só pode ser jogada logo após outra de mesmo tipo ou que tenha a mesma cor"} />
+                        <CartaInfo img={"src/assets/azulMais2.png"} nome={"Comprar Duas Cartas"} texto={"Faz o próximo jogador comprar 2 cartas. Assim como as cartas numeradas, esta carta pode ser jogada logo após outra do mesmo tipo ou que tenha a mesma cor"} />
+                        <CartaInfo img={"src/assets/mudarCor.png"} nome={"Curinga"} texto={"Permite ao jogador que jogou-a escolher com qual cor o jogo deve prosseguir"} />
+                        <CartaInfo img={"src/assets/amareloBloquear.png"} nome={"Bloquear"} texto={"Impede o próximo jogador de fazer sua jogada. Também só pode ser jogada logo após outra de mesmo tipo ou que tenha a mesma cor"} />
+                        <CartaInfo img={"src/assets/vermelha0.png"} nome={"Cartas de cor numeradas"} texto={"Cartas mais comuns do jogo, podem ser jogadas combinando tanto a cor quanto o valor"} />
+                    </div>
+                </div>
+
             </div>
-            <h2>Penalidades</h2>
-            <p><strong>1.</strong> Caso o jogador fique com uma carta na mão e não grite UNO, ele deve comprar 2 cartas. Entretanto, é necessário que alguém perceba e acuse antes do próximo jogador começar a jogada</p>
-            <p><strong>2.</strong> Caso o jogador jogue uma carta errada no monte e alguém perceba, ele deve retirar a carta e comprar duas do monte</p>
-            <p><strong>3.</strong> Jogadores que derem palpites devem comprar duas cartas do monte</p>
 
         </div>
 
--- src/pages/PaginaSobre/PaginaSobre.jsx
+++ src/pages/PaginaSobre/PaginaSobre.jsx
@@ -8,7 +8,9 @@ export default function PaginaSobre() {
             <div className="aboutContent">
                 <img src="https://s.zst.com.br/cms-assets/2022/01/regras-do-uno-capa.webp" alt=""/>
             </div>
-            
+            <p>Lançado em 1971, UNO é um jogo que fez parte da vida de muitas pessoas ao redor do mundo, contabilizando mais de 150 milhões de cópias em mais de 80 países. </p>
+            <p>A história do jogo é bastante curiosa: ele surgiu de uma briga dentro da família do barbeiro Merle Robbins. Eles estavam jogando o jogo "Oito Maluco" e tiveram discordâncias quanto às regras, e Merle, para apaziguar a situação, criou um novo baralho baseado em "Oito Maluco". A família gostou do novo modelo e logo viu uma oportunidade de lucrar com a invenção, a qual logo viria a ser chamada de "UNO".</p>
+            <p>UNO tornou-se, então, uma das marcas mais bem-sucedidas dos EUA e coleciona variações quanto ao estilo de jogo, indo desde o virtual até um modelo de empilar blocos numa torre.</p>    
             
         </div>
     )
